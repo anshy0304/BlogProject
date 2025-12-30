3128e8adb292e4b60005d5ea92e7888aaf9cb715
anshy0304 <anshy030405@gmail.com>
2025-12-30 21:51:51 +0530

    Store Configuration

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,5 +1,5 @@
 import conf from "../conf/conf";
-import { Client, ID, TablesDB ,Storage } from "appwrite";
+import { Client, ID, TablesDB ,Storage, Query } from "appwrite";
 
 export class Service{
     client = new Client()
@@ -17,10 +17,10 @@ export class Service{
 
     async createPost({title, slug, content, featuredImage, status, userId}){
         try {
-            return await this.database.TablesDB.createRow({
-                databaseId:conf.appwriteUrl,
-                tableId:conf.collectionId,
-                slug,
+            return await this.database.createRow({
+                databaseId:conf.databaseId,
+                tableId:conf.tableId,
+                rowId:slug,
                 data:{
                 title,
                 content,
@@ -34,6 +34,100 @@ export class Service{
         }
     }
 
+    async updatePost(slug,{title , content , featuredImage , status , userId}){
+        try {
+            return await this.database.updateRow({
+                
+                databaseId:conf.databaseId,
+                tableId:conf.tableId,
+                rowId:slug,
+                data:{
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                  }
+        })
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.database.deleteRow(
+               { 
+                databaseId:conf.databaseId,
+                tableId:conf.tableId,
+                rowId:slug
+            }
+            )
+            return true
+        } catch (error) {
+            throw error
+        }
+    }
+    
+    async getPost(slug) {
+        try {
+            return await this.database.getRow({
+                databaseId:conf.databaseId,
+                tableId:conf.tableId,
+                rowId:slug
+            })
+        } catch (error) {
+            throw error
+        }
+    }
+    async getPosts(query = [Query.equal("status","active")]){
+        return await this.database.listRows({
+            databaseId: conf.databaseId,
+            tableId: conf.tableId,
+            queries: query
+        });
+    }
+
+
+    //file upload
+
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile({
+                bucketId: conf.bucketId,
+                fileId: ID.unique(),
+                file: file
+            });
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile({
+                bucketId: conf.bucketId,
+                fileId: fileId
+                });
+            return true
+        } catch (error) {
+            throw error
+        }
+    }
+    getFilePreview(fileId) {
+        try {
+            return this.bucket.getFilePreview({
+                bucketId: conf.bucketId,
+                fileId: fileId,
+            });
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
+
+
 const service = new Service()
+
+export default service
