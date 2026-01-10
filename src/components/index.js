e2fcd68b6625a6f6a2e7a119002227702b62b0f3
anshy0304 <anshy030405@gmail.com>
2026-01-10 22:57:37 +0530

    Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-import Footer from "./Footer";
-import Header from "./Header";
-
-
-export {Header,Footer}
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./Header/LogoutButton";
+export {Header,Footer,Container,Logo,LogoutButton}
