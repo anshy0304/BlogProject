026950a5676a22b9eac011f922df4c998439cc20
anshy0304 <anshy030405@gmail.com>
2026-01-18 19:58:44 +0530

    Fix import path for PostCard component in index.js

--- src/components/index.js
+++ src/components/index.js
@@ -10,6 +10,6 @@ import RTE from "./RTE";
 import Signup from './Signup'
 import Login from './Login'
 import PostForm from "./post-form/PostForm";
-import PostCard from "./Postcard";
+import PostCard from "./PostCard";
 import AuthLayout from './AuthLayout'
 export {Header,Footer,Container,Logo,LogoutButton,Button,Input,RTE,Select,Login,Signup,PostForm,PostCard,AuthLayout}
